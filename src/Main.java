64bdf6eb4dc00d1eeb6ffca646391d87178ce220
Noor Hassan <noor.hassan13@yahoo.com>
2020-08-28 17:03:15 -0500

    list of TODOS

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        /*
+        2-4 player lobbies created by the player
+        present player w a menu
+        length defined messages
+        players land in first avalaible lobby
+        time input
+        usernames
+        history
+        need a way for players to call uno
+         */
     }
 }
