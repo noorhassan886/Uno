a70edc54d07c43677c56571f55ab194bb77a6efc
Noor Hassan <noor.hassan13@yahoo.com>
2021-06-26 11:54:54 -0500

    Corrections for game thread

--- src/Server/UnoGameThread.java
+++ src/Server/UnoGameThread.java
@@ -9,40 +9,31 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class UnoGameThread extends Thread {
 
     // Deck of cards to draw from
-    private Deck drawFrom;
+    private final Deck drawFrom;
     // Deck/pile of cards that are played
-    private Deck discardPile;
-    // Representation of each player, including hand
-    private PlayerHand[] hands;
-    // Tack players
-    private SocketWrapper[] players;
-    // Rules
+    private final Deck discardPile;
+    // Representation of each player, including their hand
+    private final PlayerHand[] hands;
+    // Track the players
+    private final SocketWrapper[] players;
 
     // Game state variables
-    private int playerTurn = 0; // index of player whose turn it is
-    private String direction = "cw"; //determining who the next player will be
-    private int plusTwoStacks = 0; // number of draw two's that are stacked
-    private int plusFourStacks = 0; // number of draw four's that are stacked
+    private int playerTurn = 0; // Index of the player whose turn it is
+    private String direction = "cw"; // Direction determining who the next player will be
+    private int plusTwoStacks = 0;  // Number of Draw 2's that are stacked
+    private int plusFourStacks = 0; // Number of Draw 4's that are stacked
 
-    public UnoGameThread(SocketWrapper... player) {
+    public UnoGameThread(SocketWrapper... players) {
         this.drawFrom = new Deck(108);
-        // Debugging
-//        for (int i = 0; i < 10; i++) {
-//            UnoCard topCard = this.drawFrom.drawCard();
-//            System.out.println(topCard.toString());
-//        }
-
         this.discardPile = new Deck(0);
-        this.players = player;
+        this.players = players;
         this.hands = new PlayerHand[this.players.length];
-        for (int i = 0; i < this.hands.length; i++) {
+        for (int i = 0; i < this.hands.length; i++)
             hands[i] = new PlayerHand();
-        }
     }
 
     @Override
     public void run() {
-        System.out.println("Starting new two player game");
         // Deal 7 cards per player
         for (PlayerHand hand : this.hands) {
             for (int i = 0; i < 7; i++) {
@@ -50,14 +41,13 @@ public class UnoGameThread extends Thread {
             }
         }
 
-        // Pick a card for the discard pile to start with
+        // pick a card for the discard pile to start with
         while (this.discardPile.getNumCards() == 0 || this.discardPile.getCard() instanceof SpecialCard) {
             // taking a card from the draw pile and putting it on the discard pile
             this.discardPile.addCard(this.drawFrom.drawCard());
         }
 
         // Tell the players what their hand and top card is
-        System.out.println("Sending players their opening hand");
         for (int i = 0; i < this.players.length; i++) {
             try {
                 players[i].send("NEW_HAND//" + hands[i].toString());
@@ -69,7 +59,7 @@ public class UnoGameThread extends Thread {
         }
         broadcastHandSizes();
 
-        //  Begin listening to players
+        // Begin listening to players
         final BlockingQueue<PlayerEvent> messageQueue = new LinkedBlockingQueue<>();
         final PlayerListeningThread[] listeningThreads = new PlayerListeningThread[this.players.length];
         for (int i = 0; i < listeningThreads.length; i++) {
@@ -88,39 +78,33 @@ public class UnoGameThread extends Thread {
 
             // If someone is trying to place a card
             if (event.getAction().equals("PLACE_CARD")) {
-                // If its the correct players turn
+                // If it is the correct player's turn
                 if (event.getId() == playerTurn) {
                     String card = event.getPayload();
                     // Convert "card" to an Uno Card
                     UnoCard convertedCard = UnoCard.fromString(card);
 
                     // Validate if card can be placed
-                    // Make one card validation method for
-                    // base Uno Compatibility and special card stacking rules
-
                     boolean canBePlaced = canPlaceCard(convertedCard, hands[event.getId()]);
-
 
                     if (canBePlaced) {
                         // Put the card on the discard pile
                         discardPile.addCard(convertedCard);
-
-                        // remove from the card from the hand
+                        // Remove the card from the hand
                         this.hands[playerTurn].remove(convertedCard);
-
-                        // send the updated hand to player
+                        // Send the updated hand to the player
                         try {
                             players[playerTurn].send("NEW_HAND//" + this.hands[playerTurn].toString());
                         } catch (IOException e) {
-                            System.out.println("Error sending updated hand to player " + e.getMessage());
+                            System.out.println("Error sending updated hand to player: " + e.getMessage());
                         }
                         broadcastHandSizes();
 
-                        // potentially make that person uno-able
-                        if(this.hands[playerTurn].getCards().size() == 1)
+                        // Potentially make that person uno-able
+                        if (this.hands[playerTurn].getCards().size() == 1)
                             this.hands[playerTurn].setCanCallUno(true);
 
-                        // tell all players abt new top card
+                        // Tell all players about the new top card
                         for (SocketWrapper player : players) {
                             try {
                                 player.send("TOP_CARD//" + this.discardPile.getCard().toString());
@@ -130,59 +114,62 @@ public class UnoGameThread extends Thread {
                         }
 
                         // Handle effects of cards
-                        if (convertedCard.getInfo().equals("Skip"))
+                        if (convertedCard.getInfo().equals("Skip")) {
                             playerTurn += direction.equals("cw") ? 1 : -1;
+                        }
+                        // Reverse
+                        else if (convertedCard.getInfo().equals("Reverse")) {
+                            // Reverse direction in 4-player games
+                            if (players.length == 4) {
+                                direction = direction.equals("cw") ? "ccw" : "cw";
+                            }
+                            // Do nothing in 2-player games
+                        }
+                        // Draw 2
+                        else if (convertedCard.getInfo().equals("+2")) {
+                            plusTwoStacks += 1;
+                        }
+                        // Draw 4
+                        else if (convertedCard.getInfo().equals("+4")) {
+                            plusFourStacks += 1;
+                        }
 
-                    } else if (convertedCard.getInfo().equals("Reverse")) {
-                        // Reverse direction for 4 player game
-                        if (players.length == 4)
-                            direction = direction.equals("cw") ? "ccw" : "cw";
+                        // Increment the current player's turn
+                        if (direction.equals("cw")) {
+                            playerTurn = (playerTurn + 1) % players.length;
+                        } else {
+                            playerTurn = (playerTurn - 1) % players.length;
+                        }
 
-                        // Do nothing for two player game
-                    } else if (convertedCard.getInfo().equals("+2")) {
-                        plusTwoStacks += 1;
-
-                    } else if (convertedCard.getInfo().equals("+4")) {
-                        plusFourStacks += 1;
+                        handleCardStacks();
                     }
-
-                    // wild color picking;
-                    // Increment current players turn
-                    if (direction.equals("cw")) {
-                        playerTurn = (playerTurn + 1) % players.length;
-                    } else {
-                        playerTurn = (playerTurn - 1) % players.length;
-                    }
-
-                    // Handing stacking cards
-                    handleCardStacks();
                 }
-
             }
 
             // If someone is trying to draw a card
-            else if(event.getAction().equals("DRAW_CARD")) {
-                if(event.getId() == playerTurn) {
-                    // only do this if a player does not have a card to place
+            else if (event.getAction().equals("DRAW_CARD")) {
+                if (event.getId() == playerTurn) {
+                    // Only do this if the player doesn't already have a card they can place
                     boolean needsToDraw = true;
                     for (UnoCard card : hands[playerTurn].getCards()) {
-                        if(canPlaceCard(card, hands[playerTurn])) {
+                        if (canPlaceCard(card, hands[playerTurn])) {
                             needsToDraw = false;
                             break;
                         }
                     }
-                    if(needsToDraw)
+
+                    if (needsToDraw)
                         addCardToPlayer(drawCardFromDeck(), playerTurn);
                 }
             }
 
             // If someone calls Uno
             else if (event.getAction().equals("CALL_UNO")) {
+                // See if they are calling Uno because of someone else
                 boolean checkingSomeoneElse = false;
                 for (int i = 0; i < hands.length; i++) {
                     if (i != event.getId()) {
                         if (hands[i].getCanCallUno()) {
-                            System.out.println("PLayer " + event.getId() + " calls Uno on player " + i);
                             checkingSomeoneElse = true;
                             hands[i].setCanCallUno(false);
                             for (int j = 0; j < 4; j++) {
@@ -191,20 +178,25 @@ public class UnoGameThread extends Thread {
                         }
                     }
                 }
+
+                // See if they are calling Uno because of themselves
                 boolean savedSelf = hands[event.getId()].getCanCallUno();
-                if(hands[event.getId()].getCanCallUno()) {
+                if (hands[event.getId()].getCanCallUno()) {
                     hands[event.getId()].setCanCallUno(false);
-                    System.out.println("Player " + event.getId() + " saved themselves");
                 }
 
-                if (checkingSomeoneElse && !savedSelf) {
+                // Penalize them if neither of the above happened
+                if (!checkingSomeoneElse && !savedSelf) {
                     for (int j = 0; j < 4; j++) {
                         addCardToPlayer(drawFrom.drawCard(), event.getId());
                     }
                 }
             }
         }
+
+        // TODO: Close things properly, kill threads properly
     }
+
     private boolean hasGameEnded() {
         for (PlayerHand hand : hands) {
             if (hand.getCards().size() == 0)
@@ -214,32 +206,32 @@ public class UnoGameThread extends Thread {
     }
 
     private void handleCardStacks() {
+        // Handle stacking card draws
         if (plusTwoStacks > 0 || plusFourStacks > 0) {
             if (plusTwoStacks > 0) {
-                // consume the stack if they do not have a +2
+                // Consume the stack only if they do not have a +2 to use
                 if (!hands[playerTurn].toString().contains("+2")) {
-                    // for each stack give them two cards
+                    // For each stack, give them 2 cards
                     for (int i = 0; i < plusTwoStacks; i++) {
                         addCardToPlayer(drawCardFromDeck(), playerTurn);
                         addCardToPlayer(drawCardFromDeck(), playerTurn);
                     }
-                    // Reset num of cards that needs to be drawn
+                    // Reset number of cards that needs to be drawn
                     plusTwoStacks = 0;
                 }
-            }
-
-        } else {
-            // consume the stack if they do not have a +2
-            if (!hands[playerTurn].toString().contains("Draw 4")) {
-                // for each stack give them two cards
-                for (int i = 0; i < plusFourStacks; i++) {
-                    addCardToPlayer(drawCardFromDeck(), playerTurn);
-                    addCardToPlayer(drawCardFromDeck(), playerTurn);
-                    addCardToPlayer(drawCardFromDeck(), playerTurn);
-                    addCardToPlayer(drawCardFromDeck(), playerTurn);
+            } else {
+                // Consume the stack only if they do not have a +4 to use
+                if (!hands[playerTurn].toString().contains("Draw 4")) {
+                    // For each stack, give them 2 cards
+                    for (int i = 0; i < plusFourStacks; i++) {
+                        addCardToPlayer(drawCardFromDeck(), playerTurn);
+                        addCardToPlayer(drawCardFromDeck(), playerTurn);
+                        addCardToPlayer(drawCardFromDeck(), playerTurn);
+                        addCardToPlayer(drawCardFromDeck(), playerTurn);
+                    }
+                    // Reset number of cards that needs to be drawn
+                    plusFourStacks = 0;
                 }
-                // Reset num of cards that needs to be drawn
-                plusFourStacks = 0;
             }
         }
     }
@@ -247,14 +239,15 @@ public class UnoGameThread extends Thread {
     private void addCardToPlayer(UnoCard card, int playerTurn) {
         // Add card to server copy of hand
         hands[playerTurn].addCard(card);
-        // That player cannot call Uno
+        // That player cannot call Uno anymore
         hands[playerTurn].setCanCallUno(false);
-        // Tell player their new hand
+        // Tell that player what their new hand is
         try {
             players[playerTurn].send("NEW_HAND//" + hands[playerTurn].toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         broadcastHandSizes();
     }
 
@@ -262,7 +255,7 @@ public class UnoGameThread extends Thread {
         // Reset if necessary
         if (drawFrom.getNumCards() == 0)
             drawFrom.reset(discardPile);
-        // Draw a card and return
+        // Draw a card and return it
         return drawFrom.drawCard();
     }
 
@@ -270,12 +263,11 @@ public class UnoGameThread extends Thread {
         boolean canBePlaced = discardPile.canPlaceCard(currentCard);
         if (!canBePlaced) return false;
 
-        // TODO: validate that they can place +2 on +2; +4 on +4; skip/reverse
         UnoCard match = null;
 
         for (UnoCard card : hand.getCards()) {
-            // If the type matches (+2, +4, number, Skip, etc.)
-            if (card.getInfo().equals(discardPile.getCard().getClass())) {
+            // If the type matches (+2, +4, number, Skip, etc)
+            if (card.getInfo().equals(discardPile.getCard().getInfo())) {
                 // Special case for duplicate regular cards where we need color to match as well
                 if (card instanceof RegularCard && card.getColor().equals(discardPile.getCard().getColor())) {
                     match = card;
@@ -292,6 +284,7 @@ public class UnoGameThread extends Thread {
         } else {
             return match.equals(currentCard);
         }
+
     }
 
     private void broadcastHandSizes() {
@@ -307,7 +300,8 @@ public class UnoGameThread extends Thread {
                 player.send("DISPLAYING_PLAYER_HANDS//" + payload);
             }
         } catch (IOException e) {
-            System.out.println("Error sending updated hand to player " + e.getMessage());
+            System.out.println("Error sending updated hand sizes to player: " + e.getMessage());
         }
     }
+
 }
